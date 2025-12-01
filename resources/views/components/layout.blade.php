644d3f9605928ce8b2f28290dcfa81de33db4aab
Aldo Martinez <aldo.martinez@my.utsa.edu>
2025-12-01 07:16:28 -0600

    extracted navigation and aside

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta content="width=device-width, initial-scale=1" name="viewport" />
+    <meta content="dark" name="color-scheme" />
+    @vite(['resources/css/app.css', 'resource/js/app.js'])
+    <title>PIXL</title>
+</head>
+<body
+    class="bg-pixl-dark text-pixl-light flex gap-8 px-4 sm:h-dvh sm:overflow-clip xl:gap-16"
+>
+ {{ $slot }}
+</body>
+</html>
---
+++ resources/views/partials/aside.blade.php
@@ -0,0 +1,106 @@
+<aside
+    class="-mx-4 my-4 hidden w-80 shrink-0 overflow-y-auto px-4 lg:block"
+>
+    <input
+        type="text"
+        placeholder="# Search an idea or pixl"
+        class="border-pixl-light/20 w-full text-sm"
+    />
+    <hr class="border-pixl-light/10 mt-2" />
+
+    <!-- Artists to Follow -->
+    <div class="border-pixl-light/20 mt-10 border p-4">
+        <h2 class="text-pixl-light/60 text-sm">Artists to Follow</h2>
+        <ol class="mt-4 flex flex-col gap-4">
+            <li class="flex items-center justify-between gap-2">
+                <div class="flex items-center gap-2.5">
+                    <img
+                        src="/images/alessia.png"
+                        alt="Avatar of Alessia"
+                        class="size-8 object-cover"
+                    />
+                    <p class="truncate text-sm">alessia_draws</p>
+                </div>
+                <button
+                    class="bg-pixl-dark/50 border-pixl/50 text-pixl hover:bg-pixl-dark/55 hover:border-pixl/55 active:bg-pixl-dark/75 active:border-pixl/75 border px-2 py-1 text-sm transition"
+                >
+                    Follow
+                </button>
+            </li>
+            <li class="flex items-center justify-between gap-2">
+                <div class="flex items-center gap-2.5">
+                    <img
+                        src="/images/adrian.png"
+                        alt="Avatar of Adrian"
+                        class="size-8 object-cover"
+                    />
+                    <p class="truncate text-sm">Mr. Anderson</p>
+                </div>
+                <button
+                    class="bg-pixl-dark/50 border-pixl/50 text-pixl hover:bg-pixl-dark/55 hover:border-pixl/55 active:bg-pixl-dark/75 active:border-pixl/75 border px-2 py-1 text-sm transition"
+                >
+                    Follow
+                </button>
+            </li>
+            <li class="flex items-center justify-between gap-2">
+                <div class="flex items-center gap-2.5">
+                    <img
+                        src="/images/mr-anderson.png"
+                        alt="Avatar of Alessia"
+                        class="size-8 object-cover"
+                    />
+                    <p class="truncate text-sm">Michael</p>
+                </div>
+                <button
+                    class="bg-pixl-dark/50 border-pixl/50 text-pixl hover:bg-pixl-dark/55 hover:border-pixl/55 active:bg-pixl-dark/75 active:border-pixl/75 border px-2 py-1 text-sm transition"
+                >
+                    Follow
+                </button>
+            </li>
+            <li class="flex items-center justify-between gap-2">
+                <div class="flex items-center gap-2.5">
+                    <img
+                        src="/images/adelle.png"
+                        alt="Avatar of Alessia"
+                        class="size-8 object-cover"
+                    />
+                    <p class="truncate text-sm">just_anne</p>
+                </div>
+                <button
+                    class="bg-pixl-dark/50 border-pixl/50 text-pixl hover:bg-pixl-dark/55 hover:border-pixl/55 active:bg-pixl-dark/75 active:border-pixl/75 border px-2 py-1 text-sm transition"
+                >
+                    Follow
+                </button>
+            </li>
+        </ol>
+        <a href="#" class="text-pixl-light/60 mt-4 inline-block text-sm"
+        >Show more</a
+        >
+    </div>
+
+    <!-- Follow idea streams -->
+    <div class="border-pixl-light/20 mt-4 border p-4">
+        <h2 class="text-pixl-light/60 text-sm">Follow Idea Streams</h2>
+        <ol class="mt-4 flex flex-col gap-2.5">
+            <li class="flex items-center justify-between gap-2">
+                <p class="truncate text-sm">Breakfast ideas and whatever</p>
+                <button
+                    class="bg-pixl-dark/50 border-pixl/50 text-pixl hover:bg-pixl-dark/55 hover:border-pixl/55 active:bg-pixl-dark/75 active:border-pixl/75 border px-1.5 py-0.5 text-2xl leading-none transition"
+                >
+                    +
+                </button>
+            </li>
+            <li class="flex items-center justify-between gap-2">
+                <p class="truncate text-sm">Quick book to read</p>
+                <button
+                    class="bg-pixl-dark/50 border-pixl/50 text-pixl hover:bg-pixl-dark/55 hover:border-pixl/55 active:bg-pixl-dark/75 active:border-pixl/75 border px-1.5 py-0.5 text-2xl leading-none transition"
+                >
+                    +
+                </button>
+            </li>
+        </ol>
+        <a href="#" class="text-pixl-light/60 mt-4 inline-block text-sm"
+        >Show more</a
+        >
+    </div>
+</aside>
