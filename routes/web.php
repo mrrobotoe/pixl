63c95ef180a9e530a91c1819edda45989d55740c
Aldo Martinez <aldo.martinez@my.utsa.edu>
2025-12-23 05:53:58 -0600

    edit config

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomeController::class)->middleware('guest');
